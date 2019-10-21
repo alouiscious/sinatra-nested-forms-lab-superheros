2fe0a4799a397fa83300e7dd32eaafa0b8fdbeba
Alouiscious Media <info@reneesoul.com>
2019-10-21 01:34:13 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,24 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        
+        erb :'./views/index'
+    end
 
+    get '/team' do
+        @team = Team.all
+        
+        erb :'views/team'
+    end
+
+    post '/team' do
+        binding.pry
+        @team = Team.new(params[:team])
+        
+
+        erb :'views/team'
+    end
+
+    
 end
